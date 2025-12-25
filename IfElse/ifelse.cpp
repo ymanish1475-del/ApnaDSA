cba0dbafe30d8a918d57ffb6442f6c4d66d600c1
ymanish1475-del <ymanish1475@gmail.com>
2025-12-25 19:51:48 +0530

    some improvement in my coding skiils

---
+++ IfElse/ifelse.cpp
@@ -0,0 +1,107 @@
+#include <iostream>78
+using namespace std;
+
+int main(){
+    // int n= -40;
+
+    // if (n>=0){
+    //     cout<<"num is positive";
+    // }else{
+    //     cout<<"num is negetive";
+    // }
+    // int age;
+    // cout<<"enter your age :";
+    // cin>>age;
+
+    // if(age>=18){
+    //     cout<<"aap video dekh sakte hea...";
+    // }else{
+    //     cout <<"aap video nahi dekh sakte";
+    // }
+
+
+    // int num;
+    // cout<<"enter your num :";
+    // cin>>num;
+
+    // if(num%2==0){
+    //     cout<<"ye even number hea...";
+    // }else{
+    //     cout <<"ye odd numer hea..";
+    // }
+
+    // int num;
+    // cout<<"enter your num :";
+    // cin>>num;
+
+    // if(num%2==1){
+    //     cout<<"ye odd number hea...";
+    // }else{
+    //     cout <<"ye even numer hea..";
+    // }
+
+    // int num;
+    // cout<<"enter your num :";
+    // cin>>num;
+
+    // if(num%5!=0){
+    //     cout<<"ye 5 se div nahi hota hea...";
+    // }else{
+    //     cout <<"ye 5 se div  hota hea..";
+    // }
+
+    int mark;
+    cout<<"enter your mark :";
+    cin>>mark;
+
+    // if(mark>=60){
+    //     cout<<"aap pass ho ";
+    //  }//else{
+    //     cout<<"aap fail ho";
+    // }
+     if(mark>90 && mark< 100){
+        cout<<" wow aapne top kiya hea";
+    }
+    else if(mark>100){
+        cout<<"aap is duniya ke nahi ho";
+    }
+
+     else if(mark>=60){
+        cout<<"aap pass ho ";
+     }
+     else if (mark<60){
+        cout<<"aap fail ho";
+     }
+     
+    // if else(mark>100){
+    //     cout<<"aap is universe se nahi ho";
+
+
+    // }
+
+    // if(100<=mark>=90){
+    //     cout<<"aapko A+ mila ...";
+    // }else if(89<mark>=80){
+    //     cout <<" aapko A mila..";
+    // }
+    // else if(79<mark>=70){
+    //     cout<<"aapko b mila ..";
+    // }
+    // else if(69<mark>=60){
+    //     cout<<"aapko b mila ..";
+    // }
+    // else if(59<mark>=50){
+    //     cout<<"aapko c mila ..";
+    // }
+    // else if(49<mark>=40){
+    //     cout<<"aap pass ho ..";
+    // }
+    // else if(mark<40){
+    //     cout<<"aap fail ho ..";
+    // }
+    // else if(mark>100){
+    //     cout<<"aap is universe se nahi  ho ..";
+    // }
+    
+}
+
---
+++ IfElse/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+if else(mark>100){
+    //     cout<<"aap is universe se nahi ho";
+
+
+    // }
