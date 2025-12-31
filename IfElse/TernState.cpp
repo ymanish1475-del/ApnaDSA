9109413b72521ed7b2b9d53d3efd5360b72c301b
ymanish1475-del <ymanish1475@gmail.com>
2025-12-31 20:34:24 +0530

    LEARMN TERNARY STATEMENT

---
+++ IfElse/TernState.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    
+    cin>>num;
+    cout<<(num>=50 ? "good" : "improve") ;
+    // if(num>=50) cout<<"good" ; cout<<"improve";
+    return 0;
+}
